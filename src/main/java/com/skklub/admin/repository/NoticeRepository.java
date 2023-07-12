5f81c5d8a10da9200a8aba1ca4f949d1b4cb9fd0
tlgur <begin1009@naver.com>
2023-07-13 02:28:23 +0900

    Detail Read

--- src/main/java/com/skklub/admin/repository/NoticeRepository.java
+++ src/main/java/com/skklub/admin/repository/NoticeRepository.java
@@ -1,8 +1,10 @@
 package com.skklub.admin.repository;
 
 import com.skklub.admin.domain.Notice;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -11,6 +13,9 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
     @EntityGraph(attributePaths = {"writer", "extraFiles"})
     Optional<Notice> findDetailById(Long id);
 
-    Optional<Notice> findTopByByCreatedAtLessThanEqualOrderByCreatedAtDesc(LocalDateTime createdAt);
+    @Query(value = "select n from Notice n where n.createdAt < :createdAt order by n.createdAt desc limit 1")
+    Optional<Notice> findTopByByCreatedAtLessThanEqualOOrderByCreatedAtDesc(@Param("createdAt") LocalDateTime createdAt);
+
+    @Query(value = "select n from Notice n where n.createdAt > :createdAt order by n.createdAt asc limit 1")
     Optional<Notice> findTopByCreatedAtGreaterThanEqualOrderByCreatedAtAsc(LocalDateTime createdAt);
 }
