af092eb1edf0465d6e581c71b520f98d4f474144
tlgur <begin1009@naver.com>
2023-07-18 01:42:58 +0900

    Command Test Done Except create Notice

--- src/main/java/com/skklub/admin/controller/NoticeController.java
+++ src/main/java/com/skklub/admin/controller/NoticeController.java
@@ -78,12 +78,14 @@ public class NoticeController {
     //파일 조회
     @GetMapping("/notice/file")
     public ResponseEntity<byte[]> getFile(@RequestParam String fileSavedName) {
-        S3DownloadDto s3DownloadDto = s3Transferer.downloadOne(new FileNames(null, fileSavedName));
+        S3DownloadDto s3DownloadDto = s3Transferer.downloadOne(new FileNames(
+                null, fileSavedName));
         String fileName = s3DownloadDto.getFileName();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
         httpHeaders.setContentLength(s3DownloadDto.getBytes().length);
         httpHeaders.setContentDispositionFormData("attachment", fileName);
+
 
         return new ResponseEntity<>(s3DownloadDto.getBytes(), httpHeaders, HttpStatus.OK);
     }
