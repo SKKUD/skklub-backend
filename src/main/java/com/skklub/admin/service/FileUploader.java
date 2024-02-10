426d468718a4624edf4fd57c4addad6d6947ca7e
tlgur <begin1009@naver.com>
2024-02-10 16:42:48 +0900

    feat : FileUploader upload 메서드 두개 인터페이스 작성 #122

--- src/main/java/com/skklub/admin/service/FileUploader.java
+++ src/main/java/com/skklub/admin/service/FileUploader.java
@@ -1,13 +1,26 @@
 package com.skklub.admin.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.skklub.admin.domain.FileName;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileUploader {
     private final AmazonS3 amazonS3;
+
+    public FileName uploadOne(MultipartFile multipartFile) {
+
+    }
+
+    public List<FileName> uploadAll(MultipartFile... multipartFiles) {
+
+    }
+
 }
